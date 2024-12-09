809ceb8eeaa26f1078809f0770cf5ce86613d60c
Billal Hossan <bh302333@gmail.com>
2024-12-09 19:03:08 +0600

    update

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -16,8 +16,8 @@ class _HomeScreenState extends State<HomeScreen> {
   late GoogleMapController mapController;
   final Set<Marker> _markers = {};
   final Set<Polyline> _polyLines = {};
+  List<LatLng> polyLinePoints = [];
   LatLng? destinationLatLng;
-  late StreamSubscription<Position> positionStreamSubscription;
 
   @override
   void initState() {
@@ -27,7 +27,6 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   void dispose() {
-    positionStreamSubscription.cancel(); // Make sure to cancel the stream when the widget is disposed
     super.dispose();
   }
 
@@ -40,18 +39,20 @@ class _HomeScreenState extends State<HomeScreen> {
       body: position == null
           ? const Center(child: CircularProgressIndicator())
           : GoogleMap(
-        initialCameraPosition: CameraPosition(
-          target: LatLng(position!.latitude, position!.longitude),
-          zoom: 15,
-        ),
-        onMapCreated: (GoogleMapController controller) {
-          mapController = controller;
-        },
-        myLocationEnabled: true,
-        markers: _markers,
-        polylines: _polyLines,
-        onTap: _onTapMap,
-      ),
+              initialCameraPosition: CameraPosition(
+                target: LatLng(position!.latitude, position!.longitude),
+                zoom: 15,
+              ),
+              onMapCreated: (GoogleMapController controller) {
+                mapController = controller;
+              },
+        mapType: MapType.normal,
+        zoomControlsEnabled: true,
+        zoomGesturesEnabled: true,
+              myLocationEnabled: true,
+              markers: _markers,
+              polylines: _polyLines,
+            ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           _getCurrentLocation();
@@ -68,69 +69,20 @@ class _HomeScreenState extends State<HomeScreen> {
     );
   }
 
-  void _onTapMap(LatLng currentLatLng) {
-    setState(() {
-      destinationLatLng = currentLatLng;
-      _markers.add(
-        Marker(
-          markerId: const MarkerId('destination'),
-          position: currentLatLng,
-          infoWindow: InfoWindow(
-            title: 'Destination',
-            snippet: '${currentLatLng.latitude},${currentLatLng.longitude}',
-          ),
-        ),
-      );
-      _getPoliline();
-    });
-  }
-
-  Future<void> _getPoliline() async {
-    if (destinationLatLng == null || position == null) return;
-
-    PolylinePoints points = PolylinePoints();
-    PolylineResult result = await points.getRouteBetweenCoordinates(
-      googleApiKey: 'YOUR_GOOGLE_MAPS_API_KEY',
-      request: PolylineRequest(
-          origin: PointLatLng(position!.latitude, position!.longitude),
-          destination: PointLatLng(
-            destinationLatLng!.latitude,
-            destinationLatLng!.longitude,
-          ),
-          mode: TravelMode.driving),
-    );
-    if (result.points.isNotEmpty) {
-      List<LatLng> polylineCoordinates = [];
-      for (var point in result.points) {
-        polylineCoordinates.add(LatLng(point.latitude, point.longitude));
-      }
-
-      setState(() {
-        _polyLines.add(Polyline(
-          polylineId: const PolylineId('route'),
-          color: Colors.blue,
-          width: 5,
-          points: polylineCoordinates,
-        ));
-      });
-    }
-  }
-
   Future<void> _getCurrentLocation() async {
     final isGranted = await _isLocationPermissionGranted();
     if (isGranted) {
       final isServiceEnabled = await _checkGpsServiceEnable();
       if (isServiceEnabled) {
-        // Start listening for location updates
-        positionStreamSubscription = Geolocator.getPositionStream(
-         locationSettings: const LocationSettings(
-           accuracy: LocationAccuracy.high,
-           timeLimit: Duration(seconds: 60),
-         )
-        ).listen((Position position) {
+        Geolocator.getPositionStream(
+            locationSettings: const LocationSettings(
+          accuracy: LocationAccuracy.high,
+          timeLimit: Duration(seconds: 30),
+        )).listen((Position position) {
           setState(() {
             this.position = position;
-            _updateLocation(position);
+            _addMarker(LatLng(position.latitude, position.longitude));
+            updatePolyline(LatLng(position.latitude, position.longitude));
           });
         });
       } else {
@@ -146,16 +98,22 @@ class _HomeScreenState extends State<HomeScreen> {
     }
   }
 
-  void _updateLocation(Position position) {
-    // Update the polyline based on the new location
-    if (destinationLatLng != null) {
-      _getPoliline();
-    }
-    // Add or update the marker for the current location
-    _addMarker(LatLng(position.latitude, position.longitude));
+  void updatePolyline(LatLng currentPoint) {
+    polyLinePoints.add(currentPoint);
+    _polyLines.clear();
+    _polyLines.add(
+      Polyline(
+        polylineId: const PolylineId('location-history'),
+        points: polyLinePoints,
+        color: Colors.blue,
+        width: 4,
+      ),
+    );
   }
 
+
   void _addMarker(LatLng latLng) {
+    _markers.clear();
     _markers.add(
       Marker(
         markerId: const MarkerId('current_location'),
